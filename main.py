a12ad226c5aab7f74f3e46fa02c80317d2826ec1
twk1010 <taywenkai97@gmail.com>
2025-10-08 11:14:49 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print("Chicken wing!")
+# main.py
+
+def add(a, b):
+    """Simple function to add two numbers"""
+    return a + b
+
+
+if __name__ == "__main__":
+    print("Example: 2 + 3 =", add(2, 3))
